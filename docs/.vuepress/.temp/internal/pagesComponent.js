58acfb84a84a3fac45c670ebc9c7cb37a570c293
unknown <meelis.mannari@tptlive.ee>
2021-03-24 12:46:24 +0200

    Logo uuendus

--- docs/.vuepress/.temp/internal/pagesComponent.js
+++ docs/.vuepress/.temp/internal/pagesComponent.js
@@ -2,8 +2,8 @@ import { defineAsyncComponent } from 'vue'
 
 export const pagesComponent = {
   "/": defineAsyncComponent(() => import(/* webpackChunkName: "v-8daa1a0e" */"C:/Users/Meelis/Documents/learn-docs/docs/.vuepress/.temp/pages/README.vue")),
-  "/git/": defineAsyncComponent(() => import(/* webpackChunkName: "v-74473916" */"C:/Users/Meelis/Documents/learn-docs/docs/.vuepress/.temp/pages/git/README.vue")),
   "/markdown/": defineAsyncComponent(() => import(/* webpackChunkName: "v-72881372" */"C:/Users/Meelis/Documents/learn-docs/docs/.vuepress/.temp/pages/markdown/README.vue")),
+  "/git/": defineAsyncComponent(() => import(/* webpackChunkName: "v-74473916" */"C:/Users/Meelis/Documents/learn-docs/docs/.vuepress/.temp/pages/git/README.vue")),
   "/prototyypimine/": defineAsyncComponent(() => import(/* webpackChunkName: "v-1ed782c4" */"C:/Users/Meelis/Documents/learn-docs/docs/.vuepress/.temp/pages/prototyypimine/README.vue")),
   "/tunnusgraafika/": defineAsyncComponent(() => import(/* webpackChunkName: "v-02fbb21a" */"C:/Users/Meelis/Documents/learn-docs/docs/.vuepress/.temp/pages/tunnusgraafika/README.vue")),
   "/404.html": defineAsyncComponent(() => import(/* webpackChunkName: "v-3706649a" */"C:/Users/Meelis/Documents/learn-docs/docs/.vuepress/.temp/pages/404.html.vue")),
